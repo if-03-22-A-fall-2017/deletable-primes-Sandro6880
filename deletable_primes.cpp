1e572666723a5fdd4e483f1a98b9541dfb4f07f6
sandro6880 <sandro6880@gmail.com>
2018-01-25 13:28:36 +0100

    Commit

--- deletable_primes.cpp
+++ deletable_primes.cpp
@@ -10,3 +10,28 @@
  * Implementation of deletable_primes.h.
  * ----------------------------------------------------------
  */
+ #include "deletable_primes.h"
+ #include <math.h>
+ unsigned long remove_digit(int index, unsigned long n)
+ {
+   int divisor = pow(10,index+1);
+   int multiplikator = pow(10,index);
+   long result = n/divisor;
+   result *= multiplikator;
+   result += n % multiplikator;
+   return result;
+ }
+ int index = 0;
+ int get_ways(unsigned long p){
+
+   int number = 0;
+   if(p == 2||p== 3||p==5||p==7)
+   {
+     return ++number;
+   }else{
+     p = remove_digit(index,p);
+     number += get_ways(p);
+   }
+
+   return number;
+ }
--- deletable_primes.h
+++ deletable_primes.h
@@ -12,5 +12,10 @@
  */
 #ifndef ___DELETABLE_PRIMES
 #define ___DELETABLE_PRIMES
+#include <stdio.h>
+#include <stdbool.h>
+unsigned long remove_digit(int index, unsigned long n);
+
+int get_ways(unsigned long p);
 
 #endif
